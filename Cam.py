8123b98be7e7017ea924eb8efb9db1ef80c8400b
Iain Rudge <rudgeiain@gmail.com>
2017-07-03 22:58:23 +1000

    Added anglefinding for MatlabGod

--- Cam.py
+++ Cam.py
@@ -1,20 +1,20 @@
 import cv2 
 import numpy as np
 #cap.release
-cap = cv2.VideoCapture(0)
-#cap = cv2.VideoCapture('C:/Users/Rudge-Top/Desktop/droid1.mp4')
+#cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('C:/Users/Rudge-Top/Desktop/lap2-1321.mov')
 
-while(1):
-#while(cap.isOpened()):
+#while(1):
+while(cap.isOpened()):
 
 
     # Take each frame
     _, frame = cap.read()
-    frame = frame[340:-1,:]
+    frame = frame[400:-1,-400:-1]
     
     #COnvert Frame to HSV
-#    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2LAB)
+    hsv = cv2.cvtColor(frame,cv2.COLOR_RGB2HSV)
+#    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2LAB)
 
     #Apply Gaussian Blur to the Frame 
     Blur = cv2.GaussianBlur(hsv,(5,5),0) #This is all that is required for a Guassian Blur 
@@ -22,26 +22,29 @@ while(1):
 #     HSV
 #==============================================================================
     #Create a Mask to Find the Colour Blue 
-#    lower_blue = np.array([105,30,30])
-#    upper_blue = np.array([140,255,255])
-#    lower_yellow =np.array([20,50,50])
-#    upper_yellow = np.array([60,255,255])
+    lower_blue = np.array([34,129,200])
+    upper_blue = np.array([109,248,250])
+    lower_yellow =np.array([20,60,50])
+    upper_yellow = np.array([55,200,255])
 #==============================================================================
 # LAB 
 #==============================================================================
-    lower_blue = np.array([6*(255/100),-5+128,-36+128])
-    upper_blue = np.array([100*(255/100),27+128,-9+128])
-    lower_yellow = np.array([37*(255/100),-10+128,50+128])
-    upper_yellow = np.array([67*(255/100),2+128,61+128])
+#    lower_blue = np.array([31*(255/100),-10+128,-45+128])
+#    upper_blue = np.array([42*(255/100),6+128,-20+128])
+#    lower_blue = np.array([31,-10,-45])
+#    upper_blue = np.array([42,6,-27])
+
+#    lower_yellow = np.array([37*(255/100),-10+128,50+128])
+#    upper_yellow = np.array([67*(255/100),2+128,61+128])
 #==============================================================================
 #     
 #==============================================================================
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
     mask2 = cv2.inRange(hsv, lower_yellow, upper_yellow)
     kernel = np.ones((5,5),np.uint8)
-    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    mask2 = cv2.morphologyEx(mask2, cv2.MORPH_OPEN, kernel)
+    mask2 = cv2.morphologyEx(mask2, cv2.MORPH_CLOSE, kernel)
+#    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 
 #    mask2 = cv2.morphologyEx(mask2, cv2.MORPH_OPEN, kernel)
 
@@ -55,7 +58,7 @@ while(1):
         idx = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)[1][0]
         idx_y = cv2.findContours(mask2,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)[1][0]
 #        print(cv2.contourArea(idx))
-        if cv2.contourArea(idx_y)<1000:
+        if cv2.contourArea(idx_y)<300:
             [vx_y,vy_y] = [1,1]
         else :
             [vx_y,vy_y,x_y,y_y] = cv2.fitLine(idx_y, cv2.DIST_L2,0,0.05,0.05)
@@ -65,7 +68,6 @@ while(1):
         else:    
             [vx,vy,x,y] = cv2.fitLine(idx, cv2.DIST_L2,0,0.1,0.1)
             print(x,y)
-        
     except:
         [vx,vy,vx_y,vy_y] = [1,0,1,0]
         pass
@@ -74,12 +76,7 @@ while(1):
     font = cv2.FONT_HERSHEY_SIMPLEX
     B_Angle = np.rad2deg(np.arctan(vy/vx))
     Y_Angle = np.rad2deg(np.arctan(vy_y/vx_y))
-#    if abs(B_Angle)<0.1 and abs(Y_Angle) < 0.1:
-#        Dir = "Straight"
-#    elif abs(B_Angle)==45 and abs(Y_Angle) ==45:
-#        Dir = "Straight"
-#    else:
-#        Dir = str((((B_Angle)+(Y_Angle))))
+#  
     if B_Angle == 45:
         DirB = 10.00
     elif B_Angle == 0:
@@ -90,10 +87,10 @@ while(1):
 #    cv2.putText(showme, "%.2f"%Y_Angle,(400,100), font, 1,(0,0,255),2,cv2.LINE_AA)
     cv2.putText(showme,"%.2f"%DirB,(250,100), font, 1,(255,255,255),2,cv2.LINE_AA)
 
-    cv2.imshow('Hardcore memery',showme)
+    cv2.imshow('Hardcore memery',res2)
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
 cap.release()
 cv2.destroyAllWindows()
-#del k
+del k
---
+++ Matlabgod.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+import cv2 
+import numpy as np
+from matplotlib import pyplot as plt
+
+#==============================================================================
+# Magical Values 
+#==============================================================================
+Er = 0.4
+
+
+
+#==============================================================================
+# Camera Setup 
+#==============================================================================
+CamBlue = cv2.VideoCapture(0)
+#CamBlue.set(3,480)
+#CamBlue.set(4,360)
+#CamYellow = cv2.VideoCapture(1)
+#CamYellow.set(3,480)
+#CamYellow.set(4,360)
+
+#==============================================================================
+#Set Magic Squares 
+#==============================================================================
+MagicB = [0,380,90,480]#(0,1) top left (2,3) Bottom Right 
+#With (X,Y)
+MagicY = [0,380,90,480]
+#CropB = [300:370,0:-1]
+#==============================================================================
+# Main 
+#==============================================================================
+while(1):
+    _, frameB = CamBlue.read()
+    _, frameY = CamBlue.read()
+
+#    _, frameY = CamYellow.read()
+    HSVB = cv2.cvtColor(frameB,cv2.COLOR_BGR2HSV)
+    HSVY = cv2.cvtColor(frameY,cv2.COLOR_BGR2HSV)
+
+#==============================================================================
+#     Showing Blue for testing 
+#==============================================================================
+    showblue = frameB
+    showyellow = frameY
+    cv2.rectangle(showblue,(MagicB[0],MagicB[1]),(MagicB[2],MagicB[3]),(0,255,0),3)
+    cv2.rectangle(showyellow,(MagicY[0],MagicY[1]),(MagicY[2],MagicY[3]),(0,255,0),3)
+    cv2.imshow('Line Up Blue',showblue)
+    cv2.imshow('Line Up Not Blue',showyellow)
+
+#==============================================================================
+# Average of selection
+#==============================================================================
+    AVB_h = np.mean(HSVB[MagicB[1]:MagicB[3],MagicB[0]:MagicB[2]][0])
+    AVB_s = np.mean(HSVB[MagicB[1]:MagicB[3],MagicB[0]:MagicB[2]][1])
+    AVB_v = np.mean(HSVB[MagicB[1]:MagicB[3],MagicB[0]:MagicB[2]][2])
+#    print([AVB_h,AVB_s,AVB_v])
+    AVY_h = np.mean(HSVY[MagicY[1]:MagicY[3],MagicY[0]:MagicY[2]][0])
+    AVY_s = np.mean(HSVY[MagicY[1]:MagicY[3],MagicY[0]:MagicY[2]][1])
+    AVY_v = np.mean(HSVY[MagicY[1]:MagicY[3],MagicY[0]:MagicY[2]][2])
+    
+    LowB =np.array([AVB_h-Er*AVB_h,AVB_s-Er*AVB_s,AVB_v-Er*AVB_v])
+    UpB = np.array([AVB_h+Er*AVB_h,AVB_s+Er*AVB_s,AVB_v+Er*AVB_v])
+    mask_b = cv2.inRange(HSVB,LowB,UpB)
+    LowY =np.array([AVY_h-Er*AVY_h,AVY_s-Er*AVY_s,AVY_v-Er*AVY_v])
+    UpY = np.array([AVY_h+Er*AVY_h,AVY_s+Er*AVY_s,AVY_v+Er*AVY_v])
+    mask_y = cv2.inRange(HSVY,LowY,UpY)
+    
+#==============================================================================
+#     Close
+#==============================================================================
+    kernel = np.ones((5,5),np.uint8)
+    mask_b = cv2.morphologyEx(mask_b, cv2.MORPH_CLOSE, kernel)
+    mask_y = cv2.morphologyEx(mask_y, cv2.MORPH_CLOSE, kernel)
+
+#==============================================================================
+#     Show
+#==============================================================================
+    resb = cv2.bitwise_and(frameY,frameY, mask= mask_b)
+    resy = cv2.bitwise_and(frameY,frameY, mask= mask_y)
+
+#    cv2.imshow('Praise Matlab',resb)
+#    cv2.imshow('Praise Matlab',resy)
+
+#==============================================================================
+# Angle magic via maths and shit
+#==============================================================================
+    crop_b = mask_b[200:300,0:-1]    
+    s1_b = np.nonzero(crop_b[0,:])[0][0] if np.sum(crop_b[0,:])>0 else 480
+    s2_b = np.nonzero(crop_b[20,:])[0][0] if np.sum(crop_b[20,:])>0 else 480
+    s3_b = np.nonzero(crop_b[40,:])[0][0] if np.sum(crop_b[40,:])>0 else 480
+    s4_b = np.nonzero(crop_b[60,:])[0][0] if np.sum(crop_b[60,:])>0 else 480
+    s5_b = np.nonzero(crop_b[80,:])[0][0] if np.sum(crop_b[80,:])>0 else 480
+    s6_b = np.nonzero(crop_b[99,:])[0][0] if np.sum(crop_b[99,:])>0 else 480
+    yb = np.array([99,80,60,40,20,0])
+    xb = np.array([s1_b,s2_b,s3_b,s4_b,s5_b,s6_b])
+    lineb = np.polyfit(xb, yb, 1)
+    angle_b = np.rad2deg(np.arctan(lineb[0])) 
+#    plt.plot(xb, yb, 'ro')
+#    plt.axis([0, 6, 0, 20])
+#    plt.show()
+    print(angle_b)
+    cv2.imshow('Praise Matlab',crop_b)
+#==============================================================================
+#     Exit Shit 
+#==============================================================================
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+CamBlue.release()
+cv2.destroyAllWindows()
+del k
+
